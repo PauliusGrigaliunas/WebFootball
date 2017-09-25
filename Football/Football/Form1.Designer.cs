afba5eb0e293fc1814f6f5d511c0ae55300a47ae
Mode <Moduxx@users.noreply.github.com>
2017-09-25 17:53:26 +0300

    pakeitem pavadinima

--- Football/Football/Form1.Designer.cs
+++ Football/Football/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Football
 {
-    partial class Form1
+    partial class MainMenu
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(491, 351);
+            this.Name = "MainMenu";
+            this.Text = "MainMenu";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
