0ee1b18ebe6c720c13e83590d767bf59aa44f851
Paulius <31667985+PauliusGrigaliunas@users.noreply.github.com>
2017-10-02 11:51:02 +0300

    UniTest

---
+++ Football/UnitTestProject/PictureTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Football;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class PictureTest
+    {
+            [TestMethod]
+            private void Piture()
+            {
+            //Access
+
+            PictureTest a = new PictureTest();
+
+            //Act
+            /*image.
+                ;
+            int sum;
+            sum = Calculator.Sum(2, 2);*/
+
+            //Assert
+            Assert.AreEqual(a, a);
+
+
+        }
+        
+    }
+}
+
---
+++ Football/UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bc86b6a8-32bd-488a-933b-74ef7de1c684")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
