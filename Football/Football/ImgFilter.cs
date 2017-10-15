6bd1c6ae7704f229c258694f831b34634ab72a94
Paulius <31667985+PauliusGrigaliunas@users.noreply.github.com>
2017-10-15 11:44:47 +0300

    create ImgFilter class

--- Football/Football/ImgFilter.cs
+++ Football/Football/ImgFilter.cs
@@ -8,11 +8,32 @@ using Emgu.CV;
 using Emgu.CV.Structure;
 using System.Threading;
 using Emgu.CV.UI;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Cuda;
+using System.Drawing;
 
 namespace Football
 {
     class ImgFilter
     {
+        Image<Bgr, byte> imgOriginal;
 
+        public ImgFilter(Image<Bgr, byte> imgOriginal)
+        {
+            this.imgOriginal = imgOriginal;
+
+        }
+
+
+        public Image<Gray, byte> GetFilteredImage()
+        {
+            Image<Gray, byte> imgSmoothed = imgOriginal.Convert<Hsv, byte>().InRange(new Hsv(0, 140, 150), new Hsv(180, 255, 255));
+
+            var erodeImage = CvInvoke.GetStructuringElement(ElementShape.Ellipse, new Size(5, 5), new Point(-1, -1));
+            CvInvoke.Erode(imgSmoothed, imgSmoothed, erodeImage, new Point(-1, -1), 1, BorderType.Reflect, default(MCvScalar));
+            var dilateImage = CvInvoke.GetStructuringElement(ElementShape.Ellipse, new Size(6, 6), new Point(-1, -1));
+            CvInvoke.Dilate(imgSmoothed, imgSmoothed, dilateImage, new Point(-1, -1), 1, BorderType.Reflect, default(MCvScalar));
+            return imgSmoothed;
+        }
     }
 }
