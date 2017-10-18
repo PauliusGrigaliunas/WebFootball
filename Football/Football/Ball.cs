a969a8cf067ea5d5398ca536a8dbc235e34785a0
Paulius <31667985+PauliusGrigaliunas@users.noreply.github.com>
2017-10-18 12:37:42 +0300

    Circle F in ball class

--- Football/Football/Ball.cs
+++ Football/Football/Ball.cs
@@ -8,55 +8,62 @@ using Emgu.CV;
 using Emgu.CV.Structure;
 using System.Threading;
 using Emgu.CV.UI;
+using System.Drawing;
 
 namespace Football
 {
     class Ball
     {
-        private int _blue;
-        private int _green;
-        private int _red;
-        private int _x;
-        private int _y;
-        
-        public int blue
+        public int _xBallPosition { get; set; }
+
+        public int blue { get; }
+
+        public int green{ get; }
+
+        public int red { get; }
+
+        public int x { get; }
+
+        public int y { get; }
+
+        int i = 0;
+        int[] xCoords = new int[99999999];
+
+        Image<Bgr, byte> imgOriginal { get; set; }
+        Image<Gray, byte> imgFiltered { get; set; }
+
+        public Ball()
         {
-            get
-            {
-                return _blue;
-            }
+
         }
 
-        public int green
-        {
-            get
-            {
-                return _green;
-            }
-        }
 
-        public int red
+ /*       public void BallPosition(Image<Bgr, byte> imgCircles,  textXYradius)
         {
-            get
-            {
-                return _red;
-            }
-        }
 
-        public int x
-        {
-            get
+            foreach (CircleF circle in GetCircles(imgFiltered))          //searching circles
             {
-                return _x;
-            }
-        }
+                if (textXYradius.Text != "") textXYradius.AppendText(Environment.NewLine);
 
-        public int y
-        {
-            get
-            {
-                return _y;
+                /*textXYradius.AppendText("ball position = x" + circle.Center.X.ToString().PadLeft(4) + ", y" + circle.Center.Y.ToString().PadLeft(4) + ", radius =" +
+                circle.Radius.ToString("###,000").PadLeft(7));
+                textXYradius.ScrollToCaret();
+
+                _xBallPosition = (int)circle.Center.X;                          // get x coordinate(center of a ball)
+                gcheck.StartStopwatch(_xBallPosition, imgOriginal.Width);       //start stopwatch to check if it is scored or not   
+                gcheck.Direction(_xBallPosition, i, xCoords); i++;              // 
+                imgCircles.Draw(circle, new Bgr(Color.Red), 3);                 //draw circles on smoothed image
             }
+        }*/
+        public CircleF[] GetCircles(Image<Gray, byte> imgGray)
+        {
+            Gray grayCircle = new Gray(12);
+            Gray cannyThreshold = new Gray(26);
+            double lAccumResolution = 2.0;
+            double minDistanceBtwCircles = 1.0;
+            int minRadius = 0;
+            int maxRadius = 10;
+            return imgGray.HoughCircles(grayCircle, cannyThreshold, lAccumResolution, minDistanceBtwCircles, minRadius, maxRadius)[0];
         }
     }
 }
--- Football/Football/Form1.cs
+++ Football/Football/Form1.cs
@@ -39,6 +39,8 @@ namespace Football
         SqlDataAdapter sa;
         DataTable dt = new DataTable();
         Picture picture = new Picture();
+        GoalsChecker gcheck;
+        Ball ball = new Ball();
 
         bool isTeamScored = false;
         int i = 0;
@@ -123,7 +125,7 @@ namespace Football
             }
         }
 
-        GoalsChecker gcheck;
+
         private void TimeTick(object sender, EventArgs e)
         {
 
@@ -149,7 +151,7 @@ namespace Football
 
         private void BallPosition(Image<Bgr, byte> imgCircles) {
 
-            foreach (CircleF circle in GetCircles(imgFiltered))          //searching circles
+            foreach (CircleF circle in ball.GetCircles(imgFiltered))          //searching circles
             {
                 if (textXYradius.Text != "") textXYradius.AppendText(Environment.NewLine);
 
@@ -174,33 +176,6 @@ namespace Football
             }
             Application.Exit();
         }
-
-        //start stopwatch
-        /*
-        private void StartStopwatch(int x)
-        {
-            if (x > 440)
-            {
-                isATeamScored = false;
-                isBTeamScored = true;
-                stopwatch.Reset();
-                stopwatch.Start();
-            }
-            else if (x < 45)
-            {
-                isBTeamScored = false;
-                isATeamScored = true;
-                stopwatch.Reset();
-                stopwatch.Start();
-            }
-            else
-            {
-                isBTeamScored = false;
-                isATeamScored = false;
-                stopwatch.Reset();
-            }
-
-        }*/
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -287,16 +262,6 @@ namespace Football
         private void Capture_ImageGrabbed1(object sender, EventArgs e)
         {
 
-        }
-        private CircleF[] GetCircles(Image<Gray, byte> imgGray)
-        {
-            Gray grayCircle = new Gray(12);
-            Gray cannyThreshold = new Gray(26);
-            double lAccumResolution = 2.0;
-            double minDistanceBtwCircles = 1.0;
-            int minRadius = 0;
-            int maxRadius = 10;
-            return imgGray.HoughCircles(grayCircle, cannyThreshold, lAccumResolution, minDistanceBtwCircles, minRadius, maxRadius)[0];
         }
 
         private void stopToolStripMenuItem1_Click(object sender, EventArgs e)
