385b98c97c2b72873be799c8fffd2dbc3fd7d5af
Emakieta <emilijalamanauskaite@gmail.com>
2017-10-18 22:42:24 +0300

    filter

--- Football/Football/FormAllTeams.cs
+++ Football/Football/FormAllTeams.cs
@@ -30,24 +30,72 @@ namespace Football
         
         public void fillData()
         {
-            
-                FootballEntities context = new FootballEntities();
-            var team = from i in context.teamTables
-                       orderby i.Victories descending
-                       select i;
-
-                BindingSource bi = new BindingSource();
-            //   bi.DataSource = context.teamTables.ToList();
-            bi.DataSource = team;
-            dataGridView1.DataSource = bi;
-                dataGridView1.Refresh();
-
            
+            FootballEntities context = new FootballEntities();
+
+              var team = from i in context.teamTables
+                         orderby i.Victories descending
+                         select new { i.Name, i.Victories, i.Goals };
 
 
+            dataGridView1.DataSource = team.ToList();
+ 
         }
 
         private void FormAllTeams_Load(object sender, EventArgs e)
+        {
+            fillData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Statistics_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void victoriesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FootballEntities context = new FootballEntities();
+
+            var team = from i in context.teamTables
+                       orderby i.Victories descending
+                       select new{i.Name, i.Victories};
+                       
+            dataGridView1.DataSource = team.ToList();
+
+        }
+
+        private void goalsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FootballEntities context = new FootballEntities();
+
+            var team = from i in context.teamTables
+                       orderby i.Victories descending
+                       select new { i.Name, i.Goals };
+
+
+            dataGridView1.DataSource = team.ToList();
+
+        }
+
+        private void victoriesGoals0ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FootballEntities context = new FootballEntities();
+
+            var team = from i in context.teamTables
+                       where i.Victories != 0
+                       where i.Goals != 0
+                       orderby i.Victories descending
+                       select new { i.Name, i.Victories, i.Goals };
+
+            dataGridView1.DataSource = team.ToList();
+        }
+
+        private void allToolStripMenuItem_Click(object sender, EventArgs e)
         {
             fillData();
         }
