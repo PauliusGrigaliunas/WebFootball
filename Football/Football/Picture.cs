821eb0b6ac4a40a33ad6a357b002743d94bced6f
Paulius <31667985+PauliusGrigaliunas@users.noreply.github.com>
2017-10-22 15:16:08 +0300

    +4 dump unittest

--- Football/Football/Picture.cs
+++ Football/Football/Picture.cs
@@ -19,7 +19,6 @@ namespace Football
     {
         //laukai
         private Image<Bgr, byte> imgInput;
-        private Image<Gray, byte> imgGray;
 
         //properties
         public Image<Bgr, byte> GetImage
@@ -29,25 +28,10 @@ namespace Football
                 return imgInput;
             }
         }
-        //--------------------------------------
-        //Sigletono ideja * 
-        //constructor * private
         public Picture()
         {
 
         }
-
-        protected static Picture _obj;
-
-        public static Picture GetObject()
-        {
-            if (_obj == null)
-            {
-                _obj = new Picture();
-            }
-            return _obj;
-        }
-
         //--------------------------------------
         public Image<Bgr, byte> TakeAPicture()
         {
@@ -70,56 +54,72 @@ namespace Football
         //--------------------------------------------------
         public virtual Image<Gray, byte> ConvertToGray()
         {
-            if (imgInput != null)
+            try
             {
                 return imgInput.Convert<Gray, byte>();
             }
-            else
+            catch (NullReferenceException)
             {
                 return null;
             }
-
         }
         //--------------------------------------------------
         public Image<Gray, byte> ConvertToCanny(int x, int y)
         {
-            Image<Gray, byte> imgCanny = new Image<Gray, byte>(imgInput.Width, imgInput.Height, new Gray(0));
-            imgCanny = imgInput.Canny(x, y);
-            return imgCanny;
+            try
+            {
+                Image<Gray, byte> imgCanny = new Image<Gray, byte>(imgInput.Width, imgInput.Height, new Gray(0));
+                imgCanny = imgInput.Canny(x, y);
+                return imgCanny;
+            }
+            catch (NullReferenceException)
+            {
+                return null;
+            }
         }
         //-----------------------------------------------------
         public Image<Gray, float> ConvertToSobel(int x, int y, int z)
         {
-            Image<Gray, byte> imgGray = imgInput.Convert<Gray, byte>();
-            Image<Gray, float> imgSobel = new Image<Gray, float>(imgInput.Width, imgInput.Height, new Gray(0));
-            imgSobel = imgGray.Sobel(x, y, z);
-            return imgSobel;
+            try
+            {
+                Image<Gray, byte> imgGray = imgInput.Convert<Gray, byte>();
+                Image<Gray, float> imgSobel = new Image<Gray, float>(imgInput.Width, imgInput.Height, new Gray(0));
+                imgSobel = imgGray.Sobel(x, y, z);
+                return imgSobel;
+            }
+            catch (NullReferenceException)
+            {
+                return null;
+            }
         }
         //-----------------------------------------------------
         public Image<Gray, float> ConvertToLaplase(int x)
         {
-            Image<Gray, byte> imgGray = imgInput.Convert<Gray, byte>();
-            Image<Gray, float> imgLaplasian = new Image<Gray, float>(imgInput.Width, imgInput.Height, new Gray(0));
-            imgLaplasian = imgGray.Laplace(3);
-            return imgLaplasian;
+            try
+            {
+                Image<Gray, byte> imgGray = imgInput.Convert<Gray, byte>();
+                Image<Gray, float> imgLaplasian = new Image<Gray, float>(imgInput.Width, imgInput.Height, new Gray(0));
+                imgLaplasian = imgGray.Laplace(3);
+                return imgLaplasian;
+            }
+            catch (NullReferenceException)
+            {
+                return null;
+            }
         }
-
+        //------------------------------------------------------
         public virtual Image<Gray, byte> ColorRange(int lowBlue, int lowGreen, int lowRed, int highBlue, int highGreen, int highRed)
         {
+            try
+            {
+                Image<Gray, Byte> imgRange = imgInput.InRange(new Bgr(lowBlue, lowGreen, lowRed), new Bgr(highBlue, highGreen, highRed));
+                return imgRange;
+            }
+            catch (NullReferenceException)
+            {
+                return null;
+            }
 
-            Image<Gray, Byte> imgRange = imgInput.InRange(new Bgr(lowBlue, lowGreen, lowRed), new Bgr(highBlue, highGreen, highRed));
-            return imgRange;
-        }
-
-        public Image<Gray, byte> GetFilteredImage()
-        {
-            Image<Gray, byte> imgSmoothed = imgInput.Convert<Hsv, byte>().InRange(new Hsv(0, 140, 150), new Hsv(180, 255, 255));
-
-            var erodeImage = CvInvoke.GetStructuringElement(ElementShape.Ellipse, new Size(5, 5), new Point(-1, -1));
-            CvInvoke.Erode(imgSmoothed, imgSmoothed, erodeImage, new Point(-1, -1), 1, Emgu.CV.CvEnum.BorderType.Reflect, default(MCvScalar));
-            var dilateImage = CvInvoke.GetStructuringElement(ElementShape.Ellipse, new Size(6, 6), new Point(-1, -1));
-            CvInvoke.Dilate(imgSmoothed, imgSmoothed, dilateImage, new Point(-1, -1), 1, Emgu.CV.CvEnum.BorderType.Reflect, default(MCvScalar));
-            return imgSmoothed;
         }
     }
 }
