72f7df2248cce1bba939bf274ebfd92ea90cd719
Tomas Kazlauskas <31916382+tomaskk@users.noreply.github.com>
2017-10-22 18:56:07 +0300

    IEnumerable / IEnumerator update

--- Football/Football/Ball.cs
+++ Football/Football/Ball.cs
@@ -21,7 +21,7 @@ namespace Football
         public int _xBallPosition { get; set; }      
         public int i { get; set; }
         public List<int> xCoordList = new List<int>();   // "List<T> is a generic collection"
-        private int _ix2, _z;
+        private int _ix2, _z, _j;
 
         public Image<Bgr, byte> imgOriginal { get; set; }
         public Image<Gray, byte> imgFiltered { get; set; }
@@ -56,10 +56,32 @@ namespace Football
                 }
                 i -= _ix2;
             }
+            
+            Display(xCoordList);
 
-            foreach (var coord in xCoordList)   // iterating through generic list
+            /* foreach (var coord in xCoordList)   // iterating through generic list   // paprastas foreach
+             {
+                 Console.WriteLine(coord);       // output ball's last 4 positions on the X axis
+             }*/
+            //Display(xCoordList);
+        }
+        /*
+        private void Display(IEnumerable<int> argument) // https://www.dotnetperls.com/ienumerable 2nd example // ienumerable
+        {
+            foreach (int value in argument)
             {
-                Console.WriteLine(coord);       // output ball's last 4 positions on the X axis
+                Console.WriteLine(value);
+            }
+            argument.GetEnumerator();
+        }*/
+
+        private void Display(List<int> list)          //ienumerator
+        {
+            IEnumerator<int> enumerator = list.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                int item = enumerator.Current;
+                Console.WriteLine(item);
             }
         }
     }
