50b40ba5948263e861e7bb236cdec6e6b922cd88
Paulius <31667985+PauliusGrigaliunas@users.noreply.github.com>
2017-12-06 01:09:32 +0200

    web_camera

---
+++ Football/Football/Hierarchy/WebCamera.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Football.Hierarchy
+{
+    public class WebCamera : Source
+    {
+        public override bool TakeASource()
+        {
+            Capture = new Emgu.CV.VideoCapture("http://192.168.8.101:8080/video");
+            return Starter();
+        }
+    }
+}
---
+++ Football/Football/Path.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Football
+{
+    partial class Path
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(33, 142);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(211, 62);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(211, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "http://192.168.8.101:8080/video";
+            // 
+            // Path
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Start);
+            this.Name = "Path";
+            this.Text = "Path";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
